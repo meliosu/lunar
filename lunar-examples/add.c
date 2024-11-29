c61d6374d54d25de289f60dc701284fa656c2323
Alexander <alex.atyurin@gmail.com>
2024-11-29 17:15:21 +0700

    delete v0

--- lunar-examples/add.c
+++
@@ -1,4 +0,0 @@
-int add(long *result, long a, long b) {
-    *result = a + b;
-    return 0;
-}
--- src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub mod compile;
-pub mod compute;
-pub mod execute;
-pub mod model;
-pub mod ops;
-pub mod traits;
-pub mod utils;
-pub mod value;
