224da2ddf64206469f420c433ad6e28bc7dd470a
Alexander <alex.atyurin@gmail.com>
2024-11-30 23:22:01 +0700

    rm v1

--- src/compiler/mod.rs
+++
@@ -1,5 +0,0 @@
-use std::path::PathBuf;
-
-pub fn compile(_c: String) -> anyhow::Result<PathBuf> {
-    todo!()
-}
--- src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub mod analyzer;
-pub mod ast;
-pub mod compiler;
-pub mod id;
-pub mod lexer;
-pub mod parser;
-pub mod runtime;
-pub mod translator;
